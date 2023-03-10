4fd90b0ba2db5fc619be53de701a96190a623ae0
danantur <gruzdewdanil@yandex.ru>
2023-03-10 07:14:20 +0300

    init commit 2

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const app = express()
 const pug = require('pug');
 const port = 3000
 
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
-  res.send(pug.compileFile('index.pug')())
+  res.send(pug.compileFile('promo.pug')())
+})
+
+app.get('/login', (req, res) => {
+  res.send(pug.compileFile('login.pug')())
+})
+
+app.get('/main', (req, res) => {
+  res.send(pug.compileFile('main.pug')())
+})
+
+app.get('/registration', (req, res) => {
+  res.send(pug.compileFile('registration.pug')())
+})
+
+app.get('/products', (req, res) => {
+  res.send(pug.compileFile('products.pug')())
+})
+
+app.get('/product', (req, res) => {
+  res.send(pug.compileFile('product.pug')())
 })
 
 app.listen(port, () => {
